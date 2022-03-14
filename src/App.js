0aa96064820586c55cae8aa30885d8940042acff
Alexander Stenberg <alstenberg@live.se>
2022-03-14 19:33:08 +0100

    Routes 2 missing Routes added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import BookedConcerts from './Pages/BookedConcerts';
 import Cart from './Pages/Cart';
+import ChosenEvent from './Pages/ChosenEvent';
 import Events from './Pages/Events';
+import Start from './Pages/Start';
 
 function App() {
   return (
     <Router>
       <main>
         <Routes>
+          <Route path="/" element={<Start />}></Route>
           <Route path="/events" element={<Events />}></Route>
+          <Route path="/chosenevent" element={<ChosenEvent />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/bookedconcerts" element={<BookedConcerts />}></Route>
         </Routes>
